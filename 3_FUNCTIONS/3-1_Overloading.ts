1c638f010bc691e7197db5701e5776730540e20a
HiImSUN <kshk08@hanyang.ac.kr>
2024-08-29 23:31:29 +0900

    overloading 예시 추가

--- 3_FUNCTIONS/3-1_Overloading.ts
+++ 3_FUNCTIONS/3-1_Overloading.ts
@@ -2,6 +2,8 @@
 // 그런 패키지와 라이브러리들은 오버로딩을 매우 많이 사용한다.
 // 그래서 우리는 이 오버로딩에 대해 잘 알고 있어야 한다.
 // 오버로딩은, 여러 개의 Call Signature를 가지고 있을 때 발생한다
+
+//ex1
 type Add2 = {
   (a: number, b: number): number;
   (a: number, b: string): number;
@@ -12,4 +14,23 @@ const add2: Add2 = (a, b) => {
     return a;
   }
   return a + b;
+};
+
+//ex2
+type Config = {
+  path: string;
+  state: object;
+};
+
+type Push = {
+  (path: string): void;
+  (config: Config): void;
+};
+
+const push: Push = (config) => {
+  if (typeof config === "string") {
+    console.log(config);
+  } else {
+    console.log(config.path, config.state);
+  }
 };
